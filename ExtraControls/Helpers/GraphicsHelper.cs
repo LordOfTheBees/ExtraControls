c7c54bfe52ac4522841289a4661c4d4eafff8949
LordOfTheBees <lordofbeehive@gmail.com>
2019-10-27 11:05:03 +0300

    First Commit

---
+++ ExtraControls/Helpers/GraphicsHelper.cs
@@ -0,0 +1,17 @@
+﻿using System.Drawing;
+
+namespace ExtraControls.Helpers
+{
+    public static class GraphicsHelper
+    {
+        public static Graphics DrawRoundedRectangle(this Graphics gfx, Color color, int penThickness, int cornerRadius)
+        {
+            return gfx;
+        }
+
+        public static Graphics FillRoundedRectangle(this Graphics gfx, Color, int cornerRadius)
+        {
+            return gfx;
+        }
+    }
+}
